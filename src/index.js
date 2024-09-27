0a6b0b6ef9378221041164922a05d143997ca44f
ayramesteodosio <ayrames.teodosio@gmail.com>
2024-09-27 19:09:44 -0300

    Foram feitas as estruturas para classificação dos Heróis

--- src/index.js
+++ src/index.js
@@ -10,36 +10,34 @@ let quantXP = [1000, 3000, 6000, 7500, 10500]
 for (let i = 0; i < nomeHeroi.length; i++){
     
     //Variavéis
-    let nome = nomeHeroi[i]
-    let xp = quantXP[i]
     let nivel;
 
     // Estrutura de descisão
-    if (xp <= 1000){
+    if (quantXP[i] <= 1000){
         nivel = "Ferro"
     }
-    else if(xp >= 1001 && xp <= 2000)  {
+    else if(quantXP[i] >= 1001 && quantXP[i] <= 2000)  {
         nivel = "Bronze"
     }
-    else if(xp >= 2001 && xp <= 5000){
+    else if(quantXP[i] >= 2001 && quantXP[i] <= 5000){
         nivel = "Prata"
     }
-    else if(xp >= 5001 && xp <= 7000){
+    else if(quantXP[i] >= 5001 && quantXP[i] <= 7000){
         nivel = "Ouro"
     }
-    else if(xp >= 7001 && xp <= 8000){
+    else if(quantXP[i] >= 7001 && quantXP[i] <= 8000){
         nivel = "Platina"
     }
-    else if(xp >= 8001 && xp <= 9000){
+    else if(quantXP[i] >= 8001 && quantXP[i] <= 9000){
         nivel = "Ascendente"
     }
-    else if(xp >= 9001 && xp <= 10000){
+    else if(quantXP[i] >= 9001 && quantXP[i] <= 10000){
         nivel = "Imortal"
     }
-    else if(xp >= 10001){
+    else if(quantXP[i] >= 10001){
         nivel = "Radiante"
     }
 
     //Saída
-    console.log('O Herói de nome '+ nome +' está no nível de '+ nivel)
+    console.log('O Herói de nome '+ nomeHeroi[i] +' está no nível de '+ nivel)
 }
